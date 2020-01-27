17e29697d264b654306f91091a114348714b6da3
ionantha Sun <mr.s.szw@gmail.com>
2020-01-27 23:06:33 +0800

    fix text

--- kanmeikan.js
+++ kanmeikan.js
@@ -9,22 +9,27 @@ const htmlToText = require('html-to-text');
 //   }
 //   console.log("The file was saved!");
 // });
+let i = 0
 
-async function ax(i) {
-  let url = `https://wap.kanmeikan.com/novel/47466/${6581424 + (i*2)}.html`
-  console.log(url)
-  let data = await axios.get(url)
-  let html = data.data
-  const text = htmlToText.fromString(html, {
-    wordwrap: 1000
-  });
-  fs.appendFile('input.txt', text, function (err) {
-    if (err) {
-      console.log('第'+ i +'节加载失败')
-    } else {
-      console.log('第'+ i +'节加载完成')
-    }
-  })
+async function ax() {
+  while (i < 184) {
+    let url = `https://wap.kanmeikan.com/novel/47466/${6581424 + (i * 2)}.html`
+    console.log(url)
+    let data = await axios.get(url)
+    let html = data.data
+    const text = htmlToText.fromString(html, {
+      wordwrap: 1000
+    });
+    fs.appendFile('input.txt', text, function (err) {
+      if (err) {
+        console.log('第' + i + '节加载失败')
+      } else {
+        console.log('第' + i + '节加载完成')
+      }
+    })
+    i++
+  }
+
   // fs.writeFile("input.txt", text, function (err) {
   //   if (err) {
   //     return console.log(err);
@@ -32,9 +37,9 @@ async function ax(i) {
   //   console.log("The file was saved!");
   // });
 }
-for ( let i = 0; i < 183; i++) {
-  ax(i)
-}
+// for (let i = 0; i < 183; i++) {
+  ax()
+// }
 // fs.readFile('input.txt', function (err, data) {
 //   if (err) {
 //       return console.error(err);
